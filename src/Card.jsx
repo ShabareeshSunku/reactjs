4f3a064f9f638144a2bab0c61ef4437de05db0bd
sabareesh sunku <sabareesh.sunku@espn.com>
2019-04-09 09:41:17 +0530

    lifecycle methods

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import './card.css';
 
 function Card(props) {
     return (
-        <div class="card">
+        <div className="card">
             <img src="https://www.w3schools.com/w3images/jeans3.jpg" alt="Denim Jeans" style={{ width: '100%' }} />
             <h1>Tailored Jeans</h1>
             <p className="price">$19.99</p>
--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import LifeCycle from './LifeCycle'
 export default class Timer extends Component {
     constructor(props) {
         super(props)
@@ -14,14 +14,19 @@ export default class Timer extends Component {
             me.setState({
                 date: new Date()
             })
-        }, 1000)
+        }, 10000)
     }
 
     render() {
         let date = this.state.date
         let color = this.props.color
         return (
-            <h1 style={{ color: color }}>{date.toString()}</h1>
+            <div>
+                <h1 style={{ color: color }}>{date.toString()}</h1>
+                {
+                    date.getSeconds()<=30 ? <LifeCycle key='lc' date={date}/> : null
+                }
+            </div>
         )
     }
 }
